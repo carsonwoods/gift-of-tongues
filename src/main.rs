3f8b06c22114972bdb6fa1a672576a51e5b09981
Carson Woods <cwoods289@gmail.com>
2022-09-28 10:48:09 -0400

    Add capability to enable/disable caching of definitions locally to avoid making network call

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use confy;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 
 #[derive(Parser, Debug)]
@@ -11,19 +13,68 @@ use std::error::Error;
 /// Dictionary Lookup Tool
 struct Args {
     /// word to lookup the definition for
-    word: String,
+    word: Option<String>,
 
     /// Show phonetic pronounciation information
     #[clap(long, short, action)]
     phonetic: bool,
+
+    /// enable local definition caching
+    #[clap(long, action)]
+    enable_caching: bool,
+
+    /// disable local definition caching
+    #[clap(long, action)]
+    disable_caching: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TonguesConfig {
+    caching: bool,
+}
+
+/// `TonguesConfig` implements `Default`
+impl ::std::default::Default for TonguesConfig {
+    fn default() -> Self {
+        Self { caching: true }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+    let cfg: TonguesConfig = confy::load("tongues", None)?;
+    println!("{:?}", cfg);
+    if args.enable_caching && args.disable_caching {
+        eprintln!("Error: you cannot enable and disable caching simultaneously");
+        std::process::exit(1);
+    } else if args.enable_caching {
+        let cfg = TonguesConfig {
+            caching: true,
+            ..cfg
+        };
+        confy::store("tongues", None, &cfg)?;
+        println!("Caching enabled");
+        std::process::exit(0);
+    } else if args.disable_caching {
+        let cfg = TonguesConfig {
+            caching: false,
+            ..cfg
+        };
+        confy::store("tongues", None, &cfg)?;
+        println!("Caching disabled");
+        std::process::exit(0);
+    }
 
-    let url = String::from("https://api.dictionaryapi.dev/api/v2/entries/en/") + &args.word;
+    let word: String = match args.word {
+        Some(word) => word,
+        None => {
+            eprintln!("Error: you have not specified a word to define");
+            std::process::exit(1);
+        }
+    };
 
+    let url = String::from("https://api.dictionaryapi.dev/api/v2/entries/en/") + &word;
     let data = reqwest::get(url).await?.json::<serde_json::Value>().await?;
 
     let word = data
